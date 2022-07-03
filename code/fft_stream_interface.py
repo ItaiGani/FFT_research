bc50a2cf05a51dbff29e439724cc78f133dff9ec
yotam zaiger <yotamzaigergit@gmail.com>
2022-07-03 14:35:35 +0300

    new fft interface

---
+++ code/fft_stream_interface.py
@@ -0,0 +1,17 @@
+class FFTStream:
+    def __init__(self, filename : str, mode : int = 0) -> None:
+        """Constructor for the FFTStream class.
+
+        Args:
+            filename (str): A '.wav' file to read from.
+            mode (int): The form of the fft calculations, default to 0 (normal mode). 
+        """
+        self.filename : str = filename
+        self.mode : int = mode
+
+    def hc(thresh : float = 0):
+        pass
+
+    def rick(*args, **kwargs):
+        pass
+
