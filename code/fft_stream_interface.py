34ae663a57b8188a51731a4266e2699b2a92ada8
yotam zaiger <yotamzaigergit@gmail.com>
2022-07-03 15:51:56 +0300

    update interface

--- code/fft_stream_interface.py
+++ code/fft_stream_interface.py
@@ -1,17 +1,28 @@
+from analyzing import guesser2_overlapping_windows
+
+
 class FFTStream:
-    def __init__(self, filename : str, mode : int = 0) -> None:
+    def __init__(self, signal : list(int), sr: int = 44100, mode : int = 0) -> None:
         """Constructor for the FFTStream class.
 
         Args:
-            filename (str): A '.wav' file to read from.
-            mode (int): The form of the fft calculations, default to 0 (normal mode). 
+            signal (list): The '.wav' file content.
+            sr (int, optional): The sample rate of the signal. Defaults to 44100.
+            mode (int, optional): The form of the hc algorithm. Defaults to 0.
         """
-        self.filename : str = filename
+        self.signal : list(int) = signal
+        self.sample_rate : int = sr
         self.mode : int = mode
 
-    def hc(thresh : float = 0):
-        pass
 
+    def hc(self, thresh : float = 0):
+        return guesser2_overlapping_windows(self.signal, self.sample_rate, 1000, 4, 11, 19.2, 1, 10000, True)
+
+    @staticmethod
     def rick(*args, **kwargs):
-        pass
+        import webbrowser
+        webbrowser.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
+
+if __name__ == '__main__':
+    FFTStream("../audio_wav/piano.wav")
