3faaada2bee2a18b9812e1428fd86d4f4bee7561
yotam zaiger <yotamzaigergit@gmail.com>
2022-07-04 13:23:11 +0300

    change interface and new default calculations.

---
+++ code/calc_hc.py
@@ -0,0 +1,16 @@
+import numpy as np
+from scipy.fft import rfft
+
+
+def calc_fft(data, thresh, sample_rate):
+    n = len(data)
+    f_hat = rfft(data, n)
+
+    f_hat = np.abs(f_hat)
+
+    dictionary = dict()
+    for i in range(len(f_hat)):
+        if f_hat[i] >= thresh:
+            dictionary[i * sample_rate / (2 * len(f_hat))] = f_hat[i]
+
+    return dictionary 
--- code/fft_stream_interface.py
+++ code/fft_stream_interface.py
@@ -1,20 +1,39 @@
 from scipy.io import wavfile
+import math
 
-class FFTStream:
-    def __init__(self, filename : str, mode : int = 0) -> None:
-        """Constructor for the FFTStream class.
+from calc_hc import calc_fft
 
+
+class FFTHC:
+    def __init__(self, filename : str, thresh : float = 0, win_size : float = 0.1, win_overlap : float = 0, mode : int = 0) -> None:
+        """
         Args:
-            signal (list): The '.wav' file content.
-            sr (int, optional): The sample rate of the signal. Defaults to 44100.
-            mode (int, optional): The form of the hc algorithm. Defaults to 0.
+            filename (str): Path for '.wav' file to read.
+            thresh (float, optional): The minimum amplitude of a coefficient that consider as heavy. Defaults to 0.
+            win_size (int, optional): The size of a window (in seconds). Defaults to 1.
+            win_overlap (int, optional): The percentage of the overlapping (between 0 to 1). Defaults to 0.
+            mode (int, optional): The fft mode to run. Defaults to 0.
         """
         self.sample_rate, self.signal = wavfile.read(filename)
+        self.thresh = thresh
+        self.win_size = win_size
+        self.win_overlap = win_overlap
         self.mode : int = mode
 
+        self.samples_per_window = self.sample_rate * self.win_size
 
-    def hc(self, thresh : float = 0):
-        pass
+
+    def calc(self):
+        overlapping_samples = self.samples_per_window * self.win_overlap
+        samples_step = int(self.samples_per_window - overlapping_samples)
+        number_of_wins = math.ceil((len(self.signal) - self.samples_per_window) / (self.samples_per_window - overlapping_samples)) + 1
+
+        res = []
+        for i in range(number_of_wins): 
+            f_hat = calc_fft(self.signal[i * samples_step : (i + 1) * samples_step], self.thresh, self.sample_rate)
+            res.append(f_hat)
+        return res
+        
 
     @staticmethod
     def rick(*args, **kwargs):
