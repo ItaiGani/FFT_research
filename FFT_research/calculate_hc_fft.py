20f41b0ee4c03e84039a98443cdf40719b0b44fa
yotam zaiger <yotamzaigergit@gmail.com>
2022-07-05 11:45:34 +0300

    Fix thresh to be relative

--- FFT_research/calculate_hc_fft.py
+++ FFT_research/calculate_hc_fft.py
@@ -1,8 +1,12 @@
+import random
+
 import numpy as np
 from scipy.fft import rfft
 
+SAMPLES_NORM = 100
 
-def calculate_fft(data, thresh, sample_rate):
+
+def calculate_fft(data, absolute_thresh, sample_rate):
     n = len(data)
     f_hat = rfft(data, n)
 
@@ -10,7 +14,19 @@ def calculate_fft(data, thresh, sample_rate):
 
     dictionary = dict()
     for i in range(len(f_hat)):
-        if f_hat[i] >= thresh:
+        if f_hat[i] >= absolute_thresh:
             dictionary[i * sample_rate / (2 * len(f_hat))] = f_hat[i]
 
-    return dictionary 
+    return dictionary
+
+
+
+def approx_norm_squared(window):
+    s = 0
+    samples = random.sample(range(len(window)), SAMPLES_NORM)
+
+    for sample in samples:
+        s += abs(window[sample])
+    
+    return s / SAMPLES_NORM
+
