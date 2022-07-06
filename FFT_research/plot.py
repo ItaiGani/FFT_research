739c5e68c540ec223ab9939c8a94464fd23ff074
Itai Gani <itaigani@mail.tau.ac.il>
2022-07-06 13:42:42 +0300

    improving the plotting

--- FFT_research/plot.py
+++ FFT_research/plot.py
@@ -2,6 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 
+
+#Constants
+close_amplitudes = 0.8
+min_frequnecy = 0
+
+
 def plot_audio_graph(iter, title = "transform fourier", scale: bool = False):
     fig, ax = plt.subplots() 
     x = []
@@ -19,8 +25,13 @@ def plot_audio_graph(iter, title = "transform fourier", scale: bool = False):
 
     transparency_list = []
     max_amp, min_amp = max(z), min(z)
+    if min_amp > max_amp * close_amplitudes:
+        f = lambda amp: min((1-close_amplitudes) * (amp-min_amp)/(max_amp-min_amp) + close_amplitudes,1)
+    else:
+        f = lambda amp: min(0.9 * (amp-min_amp)/(max_amp-min_amp) + 0.1,1)
     for amp in z:
-        transparency_list.append(0.9 * (amp-min_amp)/(max_amp-min_amp) + 0.1)
+        transparency_list.append(f(amp))
+    x.append(0), y.append(0), z.append(0), transparency_list.append(0)
     sc = ax.scatter(x, y, c = z, alpha = transparency_list)
     ax.set_xlabel("windows")
     if(scale):
@@ -30,9 +41,8 @@ def plot_audio_graph(iter, title = "transform fourier", scale: bool = False):
     ax.set_title(title)
     cbar = fig.colorbar(sc)
     cbar.set_label("Amplitude", loc='center')
-    yabs_max = max(y)
     if(scale):
-        ax.set_ylim(ymin=0, ymax = yabs_max + 1)
+        ax.set_ylim(ymin = min_frequnecy, ymax = max(y) + 1)
     else:
-        ax.set_ylim(ymin=0, ymax = yabs_max + 150)
+        ax.set_ylim(ymin = min_frequnecy, ymax = max(y) + 150)
     plt.show()
--- FFT_research/testing/testing_files.py
+++ FFT_research/testing/testing_files.py
@@ -7,6 +7,6 @@ sys.path.append("../FFT_research")
 from FFT_research.plot import *
 from FFT_research.fft_stream_interface import *
 
-file = FFTHC(r'FFT_research\testing\audio_wav\(500, 8192).wav', thresh=0.5)
+file = FFTHC(r'FFT_research\testing\audio_wav\(200, 4096), (300, 4096).wav', thresh=1)
 g = file.calculate()
-FFTHC.plot(g)
+FFTHC.plot(g, title = "Mulim" ,scale = True)
